4ff24c48d177d1a0998a68e13e1e6149789c703f
louis.sussan@gmail.com <louis.sussan@gmail.com>
2017-05-13 12:48:41 -0700

    updated javascript

--- assets/Javascript/javascript.js
+++ assets/Javascript/javascript.js
@@ -1,9 +1,8 @@
-var psychicLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var psychicLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 // Variables for tracking our wins, losses and ties. They begin at 0.
-var wins = 0;
-var losses = 0;
+var youWin = 0;
+var youLose = 0;
 var guessesLeft = 10;
 //array to capture user input
 var guessedLetters = []
@@ -15,3 +14,32 @@ document.onkeyup = function(event) {
     var userGuess = event.key;
     //capture the letters entered
     guessedLetters.push(userGuess);
+
+    var computerGuess = psychicLetters[Math.floor(Math.random() * psychicLetters.length)];
+
+    //point added if guess correct
+    if (userGuess === computerGuess); {
+      youWin++;
+      guessesLeft = 9;
+    }
+
+    //if it doesnt match you lose a guess
+    else {
+      guessesLeft --;
+
+    }
+
+    if (guessesLeft === 0); {
+      youLose++;
+      guessesLeft = 9;
+
+    }
+
+    var html = "<p>Guess what letter I'm thinking of: </p>" +
+      "<p>Wins: " + youWin + "</p>" +
+      "<p>Loses: " + youLose + "</p>" +
+      "<p>Guesses Remaining: " + guessesLeft + "</p>" +
+      "<p>Guessed Letter: " + guessedLetters.join(', ') + "<p>" ;
+
+      document.getElementById("results").innerHTML = html;
+  }
